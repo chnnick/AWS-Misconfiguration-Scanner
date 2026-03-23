f57f3aa771432d00a862a5eb4013008ba1793f2d
chnnick <nickchenschool@gmail.com>
2026-03-23 15:40:33 -0400

    getting rid of env requirements

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,13 +5,10 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from scanner.router import router as scanners_router
 
-from app.config import require_aws_env
-
 log = logging.getLogger("uvicorn.error")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    require_aws_env()
     port = os.environ.get("PORT", "8000")
     host = os.environ.get("API_HOST", "127.0.0.1")
     base = f"http://{host}:{port}"
